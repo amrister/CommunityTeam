502e352bc76372957eee71a324c5eb635b3ce3a3
amrister <amr.ister20@gmail.com>
2019-04-24 00:02:55 +0200

    DataBase Add ( Not External )

---
+++ api-data/src/main/java/arenaposting/com/comments/CommentRepository.java
@@ -0,0 +1,8 @@
+package arenaposting.com.comments;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentRepository extends CrudRepository<Comment,Integer>{
+	
+
+}
